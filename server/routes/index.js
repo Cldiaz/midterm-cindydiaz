6c9730be874af6bd21addce7b18a7b10d60e0dd2
Cldiaz <cindy.liliana.diaz@hotmail.com>
2016-02-25 13:25:10 -0500

    Fixed navbar and added view for contact page

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,20 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Landing' });
 });
-/* GET About page. */
-router.get('/about', function (req, res, next) {
-    res.render('index', { title: 'About' });
+/* GET People Page. */
+router.get('/people', function (req, res, next) {
+    res.render('index', { title: 'People' });
 });
-/* GET projects page. */
-router.get('/projects', function (req, res, next) {
-    res.render('index', { title: 'Projects' });
-});
-/* GET service page. */
-router.get('/services', function (req, res, next) {
-    res.render('index', { title: 'Services' });
-});
+
 /* GET contact page. */
 router.get('/contact', function (req, res, next) {
     res.render('contact', { title: 'Contact' });
